9202c6309a1d513471cb42a302922d6e88deec11
ojy <dsignshelter@naver.com>
2022-11-16 12:14:10 +0900

    시작화면,로그인화면 만듬

---
+++ app/src/main/java/com/example/streetpotholefinder/FirtstViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.streetpotholefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirtstViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_firtst_view)
+    }
+}
