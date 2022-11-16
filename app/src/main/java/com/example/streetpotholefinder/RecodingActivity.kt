0e2cd0637fcebbdbaf78b46c26bd78d70e16f3db
ojy <dsignshelter@naver.com>
2022-11-16 11:17:16 +0900

    메인,촬영중,촬영결과 뷰 만들었음

---
+++ app/src/main/java/com/example/streetpotholefinder/RecodingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.streetpotholefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecodingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recoding)
+    }
+}
