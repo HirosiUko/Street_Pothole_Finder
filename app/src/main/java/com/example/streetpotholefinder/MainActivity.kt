e17ba19a0cbffc57921b9b89facc3eacb03c0b8e
woong0997 <woong0997@gmail.com>
2022-11-17 09:02:10 +0900

    2022 11 16 night

--- app/src/main/java/com/example/streetpotholefinder/MainActivity.kt
+++ app/src/main/java/com/example/streetpotholefinder/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.streetpotholefinder
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn_startrecode = findViewById<ImageView>(R.id.btn_startrecode)
-        btn_startrecode.setOnClickListener {
+        val btnStartRecord = findViewById<ImageView>(R.id.btnStartRecord)
+        btnStartRecord.setOnClickListener {
             val intent = Intent(this, CameraXAPP::class.java)
             startActivity(intent)
         }
