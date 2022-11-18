e65ee20897cdba00402461e61ed65ac8e6889c44
ojy <dsignshelter@naver.com>
2022-11-18 17:56:36 +0900

    마이페이지추가

---
+++ app/src/main/java/com/example/streetpotholefinder/MypageActivity.kt
@@ -0,0 +1,16 @@
+package com.example.streetpotholefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MypageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage)
+
+
+        val profileimg = findViewById<ImageView>(R.id.profileimg)
+
+    }
+}
