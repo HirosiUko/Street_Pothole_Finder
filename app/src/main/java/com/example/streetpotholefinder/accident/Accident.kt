0fa3cbf88da7d568e5658dc024c6a5b27cfa3d7a
HirosiUko <53636578+HirosiUko@users.noreply.github.com>
2022-11-28 11:24:57 +0900

    - Accident class 수정2

--- app/src/main/java/com/example/streetpotholefinder/accident/Accident.kt
+++ app/src/main/java/com/example/streetpotholefinder/accident/Accident.kt
@@ -1,37 +1,30 @@
 package com.example.streetpotholefinder.accident
 
-import android.media.Image
-import android.telephony.CarrierConfigManager.Gps
+import android.graphics.Bitmap
+import android.location.Location
 import java.io.Serializable
-import java.sql.Time
+import java.time.LocalDateTime
 
-data class Accident (
-    val portholes : Array<Porthole>,
-    val cracks : Array<Crack>
+data class Accident(
+    val portholes: Array<Porthole>,
+    val cracks: Array<Crack>,
+    val issueTime: LocalDateTime
 ) : Serializable
 
-interface Issues {
-    val image : Image
-        get() {
-            TODO()
-        }
-    val gpsInfo : Gps
-        get() {
-            TODO()
-        }
-    val issueTime : Time
-        get() {
-            TODO()
-        }
+open class Issues(_image: Bitmap, _gpsInfo: Location, _issueTime: LocalDateTime ) {
+    val image: Bitmap = _image
+    val gpsInfo: Location = _gpsInfo
+    val issueTime: LocalDateTime  = _issueTime
 }
 
 //val accident : Accident = null
 //accident.portholes.lenght
 
-class Porthole : Issues {
-
+class Porthole(_image: Bitmap, _gpsInfo: Location, _issueTime: LocalDateTime ) :
+    Issues(_image, _gpsInfo, _issueTime) {
 }
 
-class Crack : Issues {
+class Crack(_image: Bitmap, _gpsInfo: Location, _issueTime: LocalDateTime ) :
+    Issues(_image, _gpsInfo, _issueTime) {
 
 }
