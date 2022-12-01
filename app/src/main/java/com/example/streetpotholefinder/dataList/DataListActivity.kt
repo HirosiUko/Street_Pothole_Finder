977067755ed2af8e6e172a7dd7d63526e7c197f9
whtmdwo <jjkl444@naver.com>
2022-12-01 10:55:12 +0900

    swipeview 삭제기능 추가

--- app/src/main/java/com/example/streetpotholefinder/dataList/DataListActivity.kt
+++ app/src/main/java/com/example/streetpotholefinder/dataList/DataListActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -16,28 +18,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.streetpotholefinder.R
 import com.example.streetpotholefinder.R.id.data_list_view
 import com.example.streetpotholefinder.RecResultActivity
-import com.example.streetpotholefinder.databinding.ActivityDataListBinding
-import java.lang.Float.max
 import java.lang.Float.min
 
 class DataListActivity : AppCompatActivity() {
     private val TAG = this.javaClass.simpleName
-    private lateinit var binding: ActivityDataListBinding
+    lateinit var adapter: DataListAdapter
+    var ContentList = mutableListOf<DataListVO>()
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_list)
 
 
+
         val rv = findViewById<RecyclerView>(data_list_view)
-        //데이터목록 리스트 선언
-        var ContentList = mutableListOf<DataListVO>()
 
         ContentList.add(DataListVO("2022년 11월 15일", "오후 2시 40분", "00:29:23", "30", "14"))
         ContentList.add(DataListVO("2022년 10월 25일", "오전 8시 18분", "00:05:22", "5", "3"))
 
         ContentList.add(DataListVO("2022년 10월 23일", "오후 3시 22분", "00:04:03", "13", "27"))
         ContentList.add(DataListVO("2022년 9월 17일", "오전 10시 33분", "00:16:45", "4", "19"))
+        adapter = DataListAdapter(ContentList)
+
+
+
+
+
 
         //데이터목록 리스트 어댑터
 //        val Adapter = DataListAdapter(this,R.layout.data_list_one_lyt, ContentList)
@@ -48,20 +54,27 @@ class DataListActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         rv.setHasFixedSize(true)
 
-        rv.adapter = DataListAdapter(ContentList)
+        adapter = DataListAdapter(ContentList)
+
+
 
         rv.addItemDecoration(
             DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
         )
 
-        val swipeHelperCallback = SwipeHelperCallback().apply { setClamp(250f) }
+        var swipeHelperCallback = SwipeHelperCallback().apply { setClamp(250f) }
         val itemTouchHelper = ItemTouchHelper(swipeHelperCallback)
         itemTouchHelper.attachToRecyclerView(rv)
 
         rv.setOnTouchListener { _, _ ->
             swipeHelperCallback.removePreviousClamp(rv)
+
             false
         }
+
+        rv.adapter = adapter
+
+    }
 
         //데이터목록 각 리스트 클릭 시 이벤트
 //        rv.onItemClickListener = AdapterView.OnItemClickListener{
@@ -72,45 +85,95 @@ class DataListActivity : AppCompatActivity() {
 //        }
 
     }
-}
+
+
+
 
 class DataListAdapter(val dataList: MutableList<DataListVO>) :
     RecyclerView.Adapter<DataListAdapter.CustomViewHolder>() {
     private lateinit var context: Context
 
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
         context = parent.context
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.data_list_one_lyt, parent, false)
         return CustomViewHolder(view).apply {
-            itemView.setOnClickListener {
+            llyt_swipe_view.setOnClickListener {
+
+
+
+
 
                 var curpos: Int = adapterPosition
                 var intent = Intent(context, RecResultActivity::class.java)
+
+
+
                 intent.putExtra("previousActivityInfo", "DataListAdapter")
                 intent.putExtra("number", curpos)
                 context.startActivity(intent)
+
+
+
+
+
+
             }
+
         }
     }
 
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
-
         holder.RecordLength.text = dataList.get(position).RecordLength
         holder.StreetDate.text = dataList.get(position).StreetDate
         holder.StreetTime.text = dataList.get(position).StreetTime
         holder.PotholeCnt.text = dataList.get(position).PotholeCnt
         holder.CrackCnt.text = dataList.get(position).CrackCnt
+//        holder.btndel.setOnClickListener {
+//            onDeleteClick.let {
+//                onDeleteClick -> onDeleteClick(this)
+//            }
+//        }
+
+//        holder.btndel.setOnClickListener {
+//            dataList.removeAt(position)
+//        }
+        holder.onDeleteClick = {
+            removeItem(it)
+        }
 
 
     }
+
+
+
+    fun removeItem(viewHolder: RecyclerView.ViewHolder) {
+        var position = viewHolder.adapterPosition
+        dataList.drop(position)
+        notifyItemRemoved(position)
+
+    }
+
+//    interface OnItemClickListener {
+//        fun OnItemClick(v: View, position: Int)
+//    }
+//    private lateinit var itemClickListener : OnItemClickListener
+//
+//    fun setItemClickListener(itemClickListener: () -> Unit) {
+//         this.itemClickListener = itemClickListener
+//
+//    }
 
     override fun getItemCount(): Int {
         return dataList.size
     }
+
+
 
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val StreetDate = itemView.findViewById<TextView>(R.id.StreetDate)
@@ -118,8 +181,25 @@ class DataListAdapter(val dataList: MutableList<DataListVO>) :
         val RecordLength = itemView.findViewById<TextView>(R.id.RecordLength)
         val PotholeCnt = itemView.findViewById<TextView>(R.id.PotholeCnt)
         val CrackCnt = itemView.findViewById<TextView>(R.id.CrackCnt)
+        val btndel = itemView.findViewById<ImageView>(R.id.btndel)
+        val llyt_swipe_view = itemView.findViewById<LinearLayout>(R.id.llyt_swipe_view)
+
+        var onDeleteClick:((RecyclerView.ViewHolder) -> Unit)? = null
+
+        init {
+            btndel.setOnClickListener {
+                onDeleteClick?.let { onDeleteClick->
+                    onDeleteClick(this)
+                }
+            }
+        }
+
+
     }
+
+
 }
+
 
 class DataListVO(
     val StreetDate: String,
@@ -136,6 +216,7 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
     private var previousPosition: Int? = null
     private var currentDx = 0f
     private var clamp = 0f
+
 
     override fun getMovementFlags(
         recyclerView: RecyclerView,
@@ -264,6 +345,8 @@ class SwipeHelperCallback : ItemTouchHelper.Callback() {
     }
 
 
-
-
 }
+
+
+
+
